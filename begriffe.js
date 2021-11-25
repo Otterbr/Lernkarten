de657a5754d1054cb89d1a63a46f2c6a8214f05c
Daniel Marx <79768755+Otterbr@users.noreply.github.com>
2021-11-25 01:31:54 +0100

    Add files via upload

---
+++ begriffe.js
@@ -0,0 +1,12 @@
+const fragen = [
+			"BeispielFrage1", 
+			"BeispielFrage2"
+			];
+		const antworten =  [
+			"Antwort1", 
+			"Antwort2"
+			];
+		const beschreibung =  [
+			"Ausf&uml;hrliche Erkl&auml;rung f&uuml;r BeispielFrage1.", 
+			"Ausf&uml;hrliche Erkl&auml;rung f&uuml;r BeispielFrage2."
+			];
